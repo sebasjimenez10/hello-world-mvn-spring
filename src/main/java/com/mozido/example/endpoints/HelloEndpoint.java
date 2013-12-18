b62e82aae552ca9c6fed659cfa06a9c92079e9a6
Sebastian Jimenez <sebastian.jimenez@mozido.com>
2013-12-18 17:09:34 -0500

    Example working via SoapUI

---
+++ src/main/java/com/mozido/example/endpoints/HelloEndpoint.java
@@ -0,0 +1,28 @@
+package com.mozido.example.endpoints;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.mozido.examples.generated.helloitems.HelloRequest;
+import com.mozido.examples.generated.helloitems.HelloResponse;
+
+@Endpoint
+public class HelloEndpoint {
+
+	@PayloadRoot(localPart = "helloRequest", namespace = "http://localhost:8080/HelloWorldSpring/hello")
+	@ResponsePayload
+	public HelloResponse handleRequest(@RequestPayload HelloRequest request)
+			throws Exception {
+
+		System.out.println("Received message: " + request.getMessage());
+
+		HelloResponse response = new HelloResponse();
+		response.setResponseMessage("Hello, you message was: "
+				+ request.getMessage());
+
+		return response;
+	}
+
+}
